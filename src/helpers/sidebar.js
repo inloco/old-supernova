22dc5aa381a73b0a5f58c9456ecaf6c9752a2e81
Marina Haack <marina.haack@inlocomedia.com>
2016-06-23 16:05:18 -0300

    fix(sidebar): Fix bind helper

--- src/helpers/sidebar.js
+++ src/helpers/sidebar.js
@@ -1,12 +1,14 @@
 import React from "react"
 
 export function bindItems(children, sidebar) {
-  return children.map((child, index) => {
-    return React.cloneElement(child, {
-      key: index,
-      handleClick: sidebar.handleClick.bind(sidebar),
-      active: sidebar.state.currentPath === child.props.href,
-      sidebar: sidebar
+  if(children !== null) {
+    return children.map((child, index) => {
+      return React.cloneElement(child, {
+        key: index,
+        handleClick: sidebar.handleClick.bind(sidebar),
+        active: sidebar.state.currentPath === child.props.href,
+        sidebar: sidebar
+      })
     })
-  })
+  }
 }
