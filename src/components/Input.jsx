4a952f039b8b6d7f6106ef68240a968650d3ecf6
Marina Haack <marina.haack@inlocomedia.com>
2016-06-23 13:16:08 -0300

    feat(input): Create input component #FRONT-78

---
+++ src/components/Input.jsx
@@ -0,0 +1,34 @@
+import React, { PropTypes } from "react"
+
+class Input extends React.Component {
+  static propTyes = {
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isRequired: PropTypes.bool.isRequired
+  }
+
+  render() {
+    const { id, type, placeholder, name, isRequired } = this.props
+    let ops = {}
+
+    if(isRequired) {
+      ops.required = "true"
+    }
+
+    return (
+      <div className="form-control-wrapper">
+        <input id={ id }
+               className="form-control"
+               type={ type }
+               placeholder={ placeholder }
+               name={ name }
+               { ...ops } />
+        <span className="material-input"></span>
+      </div>
+    )
+  }
+}
+
+export default Input
---
+++ tests/components/Input-test.js
@@ -0,0 +1,57 @@
+jest.unmock("../../src/components/Input")
+
+import React from "react"
+import ReactDOM from "react-dom"
+import TestUtils from "react-addons-test-utils"
+import Input from "../../src/components/Input"
+
+describe("Input", () => {
+  const input = TestUtils.renderIntoDocument(
+    <Input id="publisher-id"
+           type="text"
+           placeholder="Digite seu nome"
+           name="publisher[nome]"
+           isRequired={ false } />
+  )
+
+  const inputNode = ReactDOM.findDOMNode(input).querySelector("input")
+
+  it("has id", () => {
+    console.log("zdfasghfgh:", inputNode.innerHTML)
+    expect(inputNode.getAttribute("id").includes("publisher-id")).toBeTruthy
+  })
+
+  it("has type", () => {
+    expect(inputNode.getAttribute("type").includes("text")).toBeTruthy
+  })
+
+  it("has placeholder", () => {
+    expect(inputNode.getAttribute("placeholder")
+                    .includes("Digite seu nome")).toBeTruthy
+  })
+
+  it("has name", () => {
+    expect(inputNode.getAttribute("name")
+                    .includes("publisher[nome]")).toBeTruthy
+  })
+
+  it("is not required", () => {
+    expect(inputNode.getAttribute("required")).toBeFalsy
+  })
+})
+
+describe("not required", () => {
+  const input = TestUtils.renderIntoDocument(
+    <Input id="publisher-id"
+           type="text"
+           placeholder="Digite seu nome"
+           name="publisher[nome]"
+           isRequired={ true } />
+  )
+
+  const inputNode = ReactDOM.findDOMNode(input).querySelector("input")
+
+  it("is required", () => {
+    expect(inputNode.getAttribute("required")).toBeTruthy
+  })
+})
