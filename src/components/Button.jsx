c1090938ff7d3720a6a29431e06bb32237061b4e
Marina Haack <marina.haack@inlocomedia.com>
2016-07-04 12:38:16 -0300

    feat(button_component): Creating button component #FRONT-228

---
+++ src/components/Button.jsx
@@ -0,0 +1,64 @@
+import React, { PropTypes } from "react"
+
+class Button extends React.Component {
+  static propTyes = {
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    size: PropTypes.string.isRequired,
+    id: PropTypes.string,
+    icon: PropTypes.string,
+    href: PropTypes.string,
+    style: PropTypes.string,
+    secondaryStyle: PropTypes.string,
+    isDisabled: PropTypes.bool
+  }
+
+  getButtonHref() {
+    const { href } = this.props
+
+    return (
+      <a href={ href }>
+        { this.getButton() }
+      </a>
+    )
+  }
+
+  getButton() {
+    const { id, type, label, icon, size, style, secondaryStyle, isDisabled } = this.props
+    let ops = {}
+
+    if(isDisabled) {
+      ops.Disabled = "true"
+    }
+
+    return (
+      <button id={ id !== undefined ? id : null }
+              type="button"
+              className={ "btn btn-" +
+                      type +
+                      " " +
+                      style +
+                      " btn-" +
+                      size +
+                      " withripple " +
+                      secondaryStyle }
+              { ...ops }>
+        <span className={ "icon " + icon }></span>
+        <span className="button-txt">
+          { label }
+        </span>
+        <div className="ripple-wrapper"></div>
+      </button>
+    )
+  }
+
+  render() {
+    const { href } = this.props
+
+    return (
+      href !== undefined ? this.getButtonHref() : this.getButton()
+    )
+  }
+}
+
+export default Button
