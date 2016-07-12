79c761ff380ba2c84f6a8b10cb70128ef296d866
Marina Haack <marina.haack@inlocomedia.com>
2016-07-12 17:45:28 -0300

    refact(icon): Refactoring icon to have no icon size #FRONT-100

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,15 +2,23 @@ import React, { PropTypes } from "react"
 
 class Icon extends React.Component {
   static propTyes = {
-    size: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired
+    name: PropTypes.string.isRequired,
+    size: PropTypes.string
+  }
+
+  getIconClass() {
+    const{ size, name } = this.props
+
+    return (
+      size !== undefined ? "icon-" + size + " " + name : "icon " + name
+    )
   }
 
   render() {
     const{ size, name } = this.props
 
     return (
-      <span className={ "icon-" + size + " " + name }/>
+      <span className={ this.getIconClass() }/>
     )
   }
 }
