758f9e4cb1598e52a9a9a1b383c6b63cba328e03
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2016-07-19 11:06:34 -0300

    feature(docs): add routes to doc files

---
+++ docs/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var engine = require('ejs-locals');
+
+app.use(express.static('./../build'));
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/globals', function (req, res) {
+  res.render('pages/globals/index');
+});
+
+app.get('/:folder/:page', function (req, res) {
+  res.render('pages/' + req.params.folder + '/' + req.params.page);
+});
+
+app.listen(8282, function () {
+  console.log('Access the doc in http://localhost:8282!');
+});
