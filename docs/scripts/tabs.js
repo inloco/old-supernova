0cd5107fda466ff4df7028dc3c26d5c093d281c3
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2016-07-25 18:57:05 -0300

    docs(tabs): add doc to tabs component

---
+++ docs/scripts/tabs.js
@@ -0,0 +1,24 @@
+var Tabs = Supernova.Tabs;
+var Tab = Supernova.Tab;
+
+var TabsExample = React.createClass({
+  render: function() {
+    return(
+      <Tabs currentTab={1}>
+        <Tab label="Opção A">
+          <div className="sn-grid--bleed-top">Conteúdo de A</div>
+        </Tab>
+
+        <Tab label="Opção B">
+          <div className="sn-grid--bleed-top">Conteúdo de B</div>
+        </Tab>
+
+        <Tab label="Opção C">
+          <div className="sn-grid--bleed-top">Conteudo de C</div>
+        </Tab>
+      </Tabs>
+    )
+  }
+});
+
+ReactDOM.render(<TabsExample />, document.getElementById("tabs-component"));
