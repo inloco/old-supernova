37da331492ae3d93485e084822445b5e72895d59
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2016-07-25 18:56:15 -0300

    feat(tabs): add tabs component

---
+++ src/components/Tabs/Tab.jsx
@@ -0,0 +1,29 @@
+import React, { PropTypes } from "react"
+
+class Tab extends React.Component {
+  static propTyes = {
+    label:       PropTypes.string.isRequired,
+    active:      PropTypes.bool,
+    handleClick: PropTypes.func
+  }
+
+  getActiveClassName() {
+    return this.props.active ? "is-active" : ""
+  }
+
+  getClassName() {
+    return `sn-tabs--item ${this.getActiveClassName()}`
+  }
+
+  render() {
+    const { handleClick, label } = this.props
+
+    return (
+      <li className={this.getActiveClassName()}>
+        <a href="#" onClick={handleClick}>{label}</a>
+      </li>
+    )
+  }
+}
+
+export default Tab
