2546a51004ab0b334afe02f0ba0c2e1d55a07064
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2016-08-03 18:14:37 -0300

    tests(form-group): add tests to form-group component

---
+++ tests/components/Form/Group-test.js
@@ -0,0 +1,27 @@
+jest.unmock("../../factories/components/form-factory")
+
+import React from "react"
+import ReactDOM from "react-dom"
+import TestUtils from "react-addons-test-utils"
+import FormFactory from "../../factories/components/form-factory"
+
+describe("Form Group", () => {
+  const factory = new FormFactory
+  const node = factory.getNode().querySelector("#form-group-factory")
+
+  it("has group class", () => {
+    expect(node.classList.contains("sn-form-group")).toBeTruthy()
+  })
+
+  it("has an label with 'Form Group Label' content", () => {
+    const label = node.querySelector(".sn-form-group__label")
+
+    expect(label.textContent).toEqual("Form Group Label")
+  })
+
+  it("has 2 checkboxes", () => {
+    const checkboxes = node.querySelectorAll(".sn-checkbox")
+
+    expect(checkboxes.length).toEqual(2)
+  })
+})
