e1b61dd8b6af3b644fe8bb128d27b5783d0b565d
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2016-08-08 13:13:08 -0300

    feat(checkbox): add attr vale

--- src/components/Form/Checkbox.jsx
+++ src/components/Form/Checkbox.jsx
@@ -2,12 +2,12 @@ import React, { PropTypes } from "react"
 
 class Checkbox extends React.Component {
   render() {
-    const { id, label, name } = this.props
+    const { id, label, name, value } = this.props
 
     return(
       <div className="sn-checkbox" id={id}>
         <label>
-          <input type="checkbox" name={name} />
+          <input type="checkbox" name={name} value={value} />
           <i className="sn-input__icon"></i>
           {label}
         </label>
