7969568519673918456889a4e24fb0e9355007fd
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2016-08-08 13:10:45 -0300

    feat(form-group): add name otpion

--- src/components/Form/Checkbox.jsx
+++ src/components/Form/Checkbox.jsx
@@ -2,12 +2,12 @@ import React, { PropTypes } from "react"
 
 class Checkbox extends React.Component {
   render() {
-    const { id, label } = this.props
+    const { id, label, name } = this.props
 
     return(
       <div className="sn-checkbox" id={id}>
         <label>
-          <input type="checkbox"/>
+          <input type="checkbox" name={name} />
           <i className="sn-input__icon"></i>
           {label}
         </label>
