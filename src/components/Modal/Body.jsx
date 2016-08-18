73f6e61485710e085501bb085e4de9baa3f4a7bc
Julio Rangel <julio@ubee.in>
2016-08-18 12:42:03 -0300

    feat(modal-type): include sn-modal--dialog type

--- src/components/Modal/Body.jsx
+++ src/components/Modal/Body.jsx
@@ -3,7 +3,7 @@ import React, { PropTypes } from "react"
 class Body extends React.Component {
   render() {
     return(
-      <div className="modal-body">{this.props.children}</div>
+      <div className="sn-modal__body">{this.props.children}</div>
     )
   }
 }
