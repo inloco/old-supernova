d6a7471bf752664482d766366c77e6c6ae20b300
marina.haack <marina.haack@inlocomedia.com>
2016-09-27 12:59:19 -0300

    feat(ballon): refact to add tooltip

--- src/components/Ballon.jsx
+++ src/components/Ballon.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from "react"
+import Tooltip from "./Info"
 
 class Ballon extends React.Component {
   static propTyes = {
@@ -6,25 +7,48 @@ class Ballon extends React.Component {
     value: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
-    addon: PropTypes.string
+    addon: PropTypes.string,
+    isUnderAudit: PropTypes.bool,
+    underAuditText: PropTypes.string
+  }
+
+  static defaultProps = {
+    isUnderAudit: false
+  }
+
+  renderBallonHeader() {
+    return (
+      <div className={ "balloon-header " + this.props.color }>
+        <span className={ `icon-24 ${ this.props.icon }` }></span>
+        { this.props.label }
+        { this.props.isUnderAudit ? this.renderAuditIcon() : null }
+      </div>
+    )
+  }
+
+  renderAuditIcon() {
+    return <Tooltip icon="icon-alert under-audit" text={ this.props.underAuditText } />
+  }
+
+  renderBallonBody() {
+    const{ addon } = this.props
+
+    return (
+      <div className="balloon-body">
+        { addon !== null ? <span className="balloon-body-currency">{ addon }</span> : null }
+        { this.props.value }
+      </div>
+    )
   }
 
   render() {
-    const{ label, value, icon, color, addon, id } = this.props
-
-     return (
-       <div className="balloon squared" id={id}>
-         <div className={ "balloon-header " + color }>
-           <span className={ "icon-24 " + icon }></span>
-           { label }
-         </div>
-         <div className="balloon-body">
-           { addon != null ? <span className="balloon-body-currency">{ addon }</span> : null }
-           { value }
-         </div>
-       </div>
-     )
- }
+    return (
+      <div className="balloon squared" id={ this.props.id }>
+        { this.renderBallonHeader() }
+        { this.renderBallonBody() }
+      </div>
+    )
+  }
 }
 
 export default Ballon
