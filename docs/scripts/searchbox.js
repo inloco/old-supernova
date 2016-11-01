d0d9efd54da8479e3ad8a53a3d4a34f1ab00f329
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2016-11-01 16:48:11 -0300

    refact(search-box): refact searchbox to multiple results

--- docs/scripts/searchbox.js
+++ docs/scripts/searchbox.js
@@ -1,13 +1,18 @@
 var SearchBox = Supernova.SearchBox;
 
+var results = [
+  { title: "O Boticário", subtitle: "O Boticário Perfumaria S.A.", info: "01.435.546/0001-34" },
+  { title: "Hyundai", subtitle: "Caoa Motors S.A.", info: "01.435.546/0001-34" }
+]
+
+
 var SearchBoxExample = React.createClass({
   render: function() {
     return(
       <SearchBox
-        label="Busque aqui"
-        helpMessage="Texto de ajuda ou informação"
-        typeMessage="info"
-      />
+          label="Busque aqui"
+          results={results}
+          filter={true}/>
     )
   }
 });
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,14 +1,179 @@
 import React, { PropTypes } from "react"
 
 class SearchBox extends React.Component {
-  getTypeMessageClassName() {
-    const { typeMessage } = this.props
+  constructor(props) {
+    super(props)
 
-    return typeMessage ? `sn-search-box__message--${typeMessage}` : ""
+    this.state = {
+      results: this.addIdsInResults(props.results),
+      expandedResults: false
+    }
   }
 
-  getHelpMessageClassName() {
-    return `sn-search-box__message ${this.getTypeMessageClassName()}`
+  addIdsInResults(results) {
+    return results.map((result, index) => {
+      return { ...result, id: index }
+    })
+  }
+
+  componentDidMount() {
+    const body = document.querySelector('body')
+
+    body.onclick = this.handleOutsideClick.bind(this)
+  }
+
+  handleOutsideClick(event) {
+    if(event.target !== this.input) {
+      this.setState({ expandedResults: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className="sn-search-box">
+        {this.renderSelectedResults()}
+
+        <input
+          ref={input => this.input = input}
+          type="text"
+          className="sn-search-box__input"
+          placeholder={this.props.label}
+          autoComplete="off"
+          onFocus={this.handleInputFocus.bind(this)}
+          onChange={this.handleInputChange.bind(this)}/>
+
+        <span className="sn-search-box__input--icon"></span>
+
+        {this.hasMinimumInputValue() && this.renderResults()}
+        {this.renderHelpMessage()}
+      </div>
+    )
+  }
+
+  renderSelectedResults() {
+    const selectedResults = this.getSelectedResults()
+
+    if(selectedResults.length === 0) return null
+
+    return (
+      <ul className="sn-search-box__selected">
+        {selectedResults.map(result => <li key={result.id}>{this.renderSelectedResultCard(result)}</li>)}
+      </ul>
+    )
+  }
+
+  getSelectedResults() {
+    return this.state.results.filter(result => result.selected)
+  }
+
+  renderSelectedResultCard(selectedResult) {
+    return (
+      <div>
+        <div className="sn-search-box__item-content">{selectedResult.title}</div>
+        <button
+          type="button"
+          className="sn-search-box__item-button"
+          onClick={this.handleUnselectClick.bind(this, selectedResult)}>
+        </button>
+      </div>
+    )
+  }
+
+  handleInputFocus() {
+    this.setState({ expandedResults: true })
+  }
+
+  handleUnselectClick(selectedResult) {
+    this.setState({
+      results: this.state.results.map(result => {
+        return selectedResult.id === result.id ? { ...result, selected: false } : result
+      })
+    })
+
+    this.input.focus()
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      results: this.state.results.map(result => {
+        const matched = result.title.toLowerCase().includes(event.target.value.toLowerCase())
+
+        return { ...result, matched }
+      })
+    })
+  }
+
+  hasMinimumInputValue() {
+    return this.input && this.input.value.length > 1
+  }
+
+  renderResults() {
+    const visibleResults = this.getVisibleResults()
+
+    return (
+      <ul
+        className="sn-search-box__results"
+        style={{ display: this.state.expandedResults ? 'block' : 'none' }}>
+        {
+          visibleResults.length === 0
+            ? this.renderEmptyMessage()
+            : visibleResults.map(result => <li key={result.id}>{this.renderResultCard(result)}</li>)
+        }
+      </ul>
+    )
+  }
+
+  getVisibleResults() {
+    return this.state.results.filter(result => !result.selected && result.matched)
+  }
+
+  renderEmptyMessage() {
+    return (
+      <li className="sn-search-box__results--no-results">
+        <div className="sn-search-box__item-content">
+          Nenhum resultado encontrado.
+        </div>
+      </li>
+    )
+  }
+
+  renderResultCard(result) {
+    return (
+      <div onClick={this.handleResultCardMouseDown.bind(this, result)}>
+        <div className="sn-search-box__item-content">
+          <div className="sn-grid sn-grid--responsive-mobile-lg sn-grid--cell-gutter">
+            <div className="sn-cell--1">
+              <span className="sn-typo--font-bold">{result.title}</span>
+            </div>
+            <div className="sn-cell--auto sn-cell--text-right">
+              <span className="sn-typo--caption sn-color--secondary-text">{result.info}</span>
+            </div>
+          </div>
+          <div className="sn-grid sn-grid--responsive-mobile-lg">
+            <div className="sn-cell--1">
+              <span className="sn-typo--caption sn-color--secondary-text">{result.subtitle}</span>
+            </div>
+          </div>
+        </div>
+
+        <button type="button" className="sn-search-box__item-button"></button>
+      </div>
+    )
+  }
+
+  handleResultCardMouseDown(result, event) {
+    this.selectResult(result)
+
+    this.input.focus()
+    this.input.value = ""
+  }
+
+  selectResult(resultToSelect) {
+    this.setState({
+      results: this.state.results.map(result => {
+        return resultToSelect.id === result.id ? { ...result, selected: true } : result
+      })
+    })
   }
 
   renderHelpMessage() {
@@ -19,16 +184,19 @@ class SearchBox extends React.Component {
     )
   }
 
-  render() {
-    return (
-      <div className="sn-search-box">
-        <input type="text" className="sn-search-box__input" placeholder={this.props.label} autoComplete="off"/>
-        <span className="sn-search-box__input--icon"></span>
-
-        {this.renderHelpMessage()}
-      </div>
-    )
+  getHelpMessageClassName() {
+    return `sn-search-box__message ${this.getTypeMessageClassName()}`
   }
+
+  getTypeMessageClassName() {
+    const { typeMessage } = this.props
+
+    return typeMessage ? `sn-search-box__message--${typeMessage}` : ""
+  }
+}
+
+SearchBox.defaultProps = {
+  results: []
 }
 
 export default SearchBox
