60a978a81eb927d42fe171d38d75a016b7294eb3
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2016-11-09 16:01:14 -0300

    fix(tests): fix dropdown tests

--- tests/factories/components/dropdown-factory.js
+++ tests/factories/components/dropdown-factory.js
@@ -1,20 +1,12 @@
-jest.unmock("./base-factory")
-jest.unmock("../../../src/components/Dropdown/Main")
-
 import React from "react"
-import Factory from "./base-factory"
 import Dropdown from "../../../src/components/Dropdown/Main"
 
-class DropdownFactory extends Factory {
-  template() {
-    return(
-      <Dropdown label="Dropdown" layout="box">
-        <a href="#">Dropdown result 1</a>
-        <a href="#">Dropdown result 2</a>
-        <a href="#">Dropdown result 3</a>
-      </Dropdown>
-    )
-  }
-}
+const DropdownFactory = (
+  <Dropdown value="Dropdown" layout="box">
+    <a href="#">Dropdown result 1</a>
+    <a href="#">Dropdown result 2</a>
+    <a href="#">Dropdown result 3</a>
+  </Dropdown>
+)
 
 export default DropdownFactory
