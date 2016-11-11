d899b0519741731c796af4eff039c55be74b151f
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2016-11-11 14:56:13 -0300

    style(seachbox/onchage): refact tests to use enzyme

--- tests/components/Searchbox-test.js
+++ tests/components/Searchbox-test.js
@@ -1,44 +1,45 @@
-jest.unmock("../factories/components/searchbox-factory")
-
 import React from "react"
-import ReactDOM from "react-dom"
-import TestUtils from "react-addons-test-utils"
-import SearchBoxFactory from "../factories/components/searchbox-factory"
+import SearchBox from "./../../src/components/SearchBox"
+import { shallow } from "enzyme"
 
 describe("Search Box", () => {
-  const factory = new SearchBoxFactory
-  const node = factory.getNode()
+  const wrapper = shallow(
+    <SearchBox
+      label="Busque aqui"
+      helpMessage="Foo"
+      typeMessage="info"/>
+  )
 
   it("has searchbox class", () => {
-    expect(node.classList.contains("sn-search-box")).toBeTruthy()
+    expect(wrapper.hasClass("sn-search-box")).toBeTruthy()
   })
 
   it("has an input", () => {
-    const input = node.querySelector("input.sn-search-box__input")
+    const input = wrapper.find("input.sn-search-box__input")
 
     expect(input).not.toBeNull()
   })
 
   it("has an input icon", () => {
-    const icon = node.querySelector(".sn-search-box__input--icon")
+    const icon = wrapper.find(".sn-search-box__input--icon")
 
     expect(icon).not.toBeNull()
   })
 
   it("has a placeholder with 'Busque aqui'", () => {
-    const input = node.querySelector("input.sn-search-box__input")
+    const input = wrapper.find("input.sn-search-box__input")
 
-    expect(input.placeholder).toEqual("Busque aqui")
+    expect(input.props().placeholder).toEqual("Busque aqui")
   })
 
   it("has a help message with 'foo' content", () => {
-    const message = node.querySelector(".sn-search-box__message")
+    const message = wrapper.find(".sn-search-box__message")
 
-    expect(message.textContent).toEqual("Foo")
+    expect(message.text()).toEqual("Foo")
   })
 
   it("has a info help message", () => {
-    const infoMessage = node.querySelector(".sn-search-box__message.sn-search-box__message--info")
+    const infoMessage = wrapper.find(".sn-search-box__message.sn-search-box__message--info")
 
     expect(infoMessage).not.toBeNull()
   })
