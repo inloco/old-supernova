82ce45831c83e4c797c67e5c492bbcbb5877a5c7
marina.haack <marina.haack@inlocomedia.com>
2016-11-17 09:50:25 -0300

    feat(Cell/Head.jsx): refact table cell to ads

--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -3,14 +3,17 @@ import React, { PropTypes } from "react"
 class Label extends React.Component {
   static propTyes = {
     value: PropTypes.string.isRequired,
-    htmlFor: PropTypes.string
+    htmlFor: PropTypes.string,
+    className: PropTypes.string
   }
 
   render() {
     const{ htmlFor, value } = this.props
 
     return (
-      <label className="control-label" htmlFor={ htmlFor ? htmlFor : null }>
+      <label
+        className={`control-label ${this.props.className}`}
+        htmlFor={ htmlFor ? htmlFor : null }>
         { value }
       </label>
     )
