023038442f95bcf10efcc81a7ab3592adb79f424
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2017-01-11 18:19:28 -0300

    feat(refact-build): remove build folder and add postinstall script

--- build/helpers/table.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.getColumnClassByType = getColumnClassByType;
-function getColumnClassByType(type) {
-  return type ? type + "-col" : null;
-}
