054ae01f74366961e639e2833ed94613ff588eb9
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2017-01-13 11:28:28 -0300

    feat(layout): add layout component

---
+++ tests/components/Layout-test.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Layout from './../../src/components/Layout'
+import { shallow } from 'enzyme'
+
+describe('Layout', () => {
+  const wrapper = shallow(
+    <Layout>
+      <h1>Cotnent</h1>
+    </Layout>
+  )
+
+  it('has layout container', () => {
+    const container = wrapper.find('.sn-layout__container')
+
+    expect(container.length).toEqual(1)
+  })
+
+  it('has layout element', () => {
+    const layout = wrapper.find('.sn-layout')
+
+    expect(layout.length).toEqual(1)
+  })
+
+  it('has h1 children', () => {
+    const children = wrapper.find('h1')
+
+    expect(children.length).toEqual(1)
+  })
+
+  describe('fixed drawer className', () => {
+    it('not have drawer', () => {
+      const layoutWithoutDrawer = wrapper.find('.sn-layout--fixed-drawer')
+
+      expect(layoutWithoutDrawer.length).toEqual(0)
+    })
+
+    it('have drawer', () => {
+      wrapper.setProps({ drawer: true })
+
+      const layoutWithDrawer = wrapper.find('.sn-layout--fixed-drawer')
+
+      expect(layoutWithDrawer.length).toEqual(1)
+    })
+  })
+
+})
