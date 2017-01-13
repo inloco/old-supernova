1151410559c4b46295062c9f16421d9bad3a749c
Bruno Quaresma <brunoquaresma@Mac-mini-de-In-2.local>
2017-01-13 12:34:52 -0300

    feat(board): add board

---
+++ tests/components/Board-test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Board from './../../src/components/Board'
+import { shallow } from 'enzyme'
+
+describe('Board', () => {
+  const wrapper = shallow(
+    <Board>
+      <h1>Board</h1>
+    </Board>
+  )
+
+  it('has Board', () => {
+    const board = wrapper.find('.sn-board')
+
+    expect(board.length).toEqual(1)
+  })
+
+  it('has h1 children', () => {
+    const children = wrapper.find('h1')
+
+    expect(children.length).toEqual(1)
+  })
+
+  it('has bleed props', () => {
+    wrapper.setProps({ bleed: true })
+
+    const boardWithBleed = wrapper.find('.sn-board__bleed-all')
+
+    expect(boardWithBleed.length).toEqual(1)
+  })
+})
