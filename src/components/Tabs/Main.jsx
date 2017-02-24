9e077c7ffcabbdd85e71dc4cb7fa0e9c7e88c868
marina.haack <marina.haack@inlocomedia.com>
2017-02-24 14:38:09 -0300

    refact(tabs): refact tabs

--- src/components/Tabs/Main.jsx
+++ src/components/Tabs/Main.jsx
@@ -1,39 +1,6 @@
-import React, { PropTypes } from "react"
+import React from 'react'
 
-class Main extends React.Component {
-  static propTypes = {
-    active: PropTypes.number,
-    children: PropTypes.node.isRequired,
-    updateWhenReceiveProps: PropTypes.bool
-  }
-
-  static defaultProps = {
-    active: 0,
-    updateWhenReceiveProps: false
-  }
-
-  constructor(props) {
-    super(props)
-
-    this.state = { active: props.active }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if(this.props.updateWhenReceiveProps) {
-      this.setState({ active: nextProps.active })
-    }
-  }
-
-  handleTabClick(index) {
-    const { onChange } = this.props
-
-    this.setState({ active: index })
-
-    if(onChange) {
-      onChange(index)
-    }
-  }
-
+class Main extends React.PureComponent {
   getTabsLabel() {
     return this.props.children.map(tab => {
       return tab.props.label
@@ -41,14 +8,14 @@ class Main extends React.Component {
   }
 
   getTabItemClassName(index) {
-    return this.state.active === index ? "is-active" : ""
+    return this.props.active === index ? 'is-active' : ''
   }
 
   renderTabsItems() {
     return this.getTabsLabel().map((label, index) => {
       return (
         <li className={this.getTabItemClassName(index)} key={index}>
-          <a href="#" onClick={this.handleTabClick.bind(this, index)}>
+          <a href="#" onClick={() => this.props.onClick(index)}>
             {label}
           </a>
         </li>
@@ -59,8 +26,8 @@ class Main extends React.Component {
   renderTabsContent() {
     return this.props.children.map((tab, index) => {
       return React.cloneElement(tab, {
-        active: this.state.active === index,
-        key:    index
+        active: this.props.active === index,
+        key: index
       })
     })
   }
@@ -71,7 +38,6 @@ class Main extends React.Component {
         <div className="sn-tabs">
           <ul>{this.renderTabsItems()}</ul>
         </div>
-
         <div className="sn-tabs__content">
           {this.renderTabsContent()}
         </div>
