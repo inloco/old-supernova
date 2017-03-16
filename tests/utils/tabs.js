c967c836f812600f1805a43f06d6f45aa347130c
marina.haack <marina.haack@inlocomedia.com>
2017-03-16 12:10:33 -0300

    test(tabs-test): adds test #FSRE-11

--- tests/utils/tabs.js
+++ tests/utils/tabs.js
@@ -1,7 +1,9 @@
 export const isShowingContent = (content, expectedContent) => {
   const currentContent = content.text()
+  const currentActiveProps = content.props().active
 
   expect(currentContent).toEqual(expectedContent)
+  expect(currentActiveProps).toBeTruthy()
 }
 
 export const isActive = tab => {
