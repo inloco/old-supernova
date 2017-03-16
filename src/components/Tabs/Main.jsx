f80e938adb12f4ec3962264331ab45c789eeb30e
marina.haack <marina.haack@inlocomedia.com>
2017-03-16 11:44:38 -0300

    Revert "feat(main): adds simple logic to tabs #FSRE-11"
    This reverts commit 3488131fc4418e3b115051e8678484aa35339979.

--- src/components/Tabs/Main.jsx
+++ src/components/Tabs/Main.jsx
@@ -1,28 +1,6 @@
 import React from 'react'
 
 class Main extends React.PureComponent {
-  static defaultProps = {
-    active: 0,
-    customBehavior: false
-  }
-
-  constructor(props) {
-    super(props)
-
-    this.handleTabClick = this.handleTabClick.bind(this)
-
-    this.state = { activeTab: props.active }
-  }
-
-  handleTabClick(index) {
-    const { onClick } = this.props
-    this.setState({ activeTab: index })
-
-    if(onClick) {
-      onClick(index)
-    }
-  }
-
   getTabsLabel() {
     return this.props.children.map(tab => {
       return tab.props.label
@@ -30,18 +8,14 @@ class Main extends React.PureComponent {
   }
 
   getTabItemClassName(index) {
-    const { customBehavior, active } = this.props
-    const { activeTab } = this.state
-    const nextActiveTab = customBehavior ? active : activeTab
-
-    return nextActiveTab === index ? 'is-active' : ''
+    return this.props.active === index ? 'is-active' : ''
   }
 
   renderTabsItems() {
     return this.getTabsLabel().map((label, index) => {
       return (
         <li className={this.getTabItemClassName(index)} key={index}>
-          <a href="#" onClick={() => this.handleTabClick(index)}>
+          <a href="#" onClick={() => this.props.onClick(index)}>
             {label}
           </a>
         </li>
@@ -50,13 +24,9 @@ class Main extends React.PureComponent {
   }
 
   renderTabsContent() {
-    const { customBehavior, active } = this.props
-    const { activeTab } = this.state
-    const nextActiveTab = customBehavior ? active : activeTab
-
     return this.props.children.map((tab, index) => {
       return React.cloneElement(tab, {
-        active: nextActiveTab === index,
+        active: this.props.active === index,
         key: index
       })
     })
--- tests/components/Tabs-test.js
+++ tests/components/Tabs-test.js
@@ -5,19 +5,34 @@ import { mount } from 'enzyme'
 import { isShowingContent, isActive } from './../utils/tabs'
 
 describe('Tabs', () => {
-  describe('Simple behavior', () => {
-    const wrapper = mount(
-      <Tabs>
-        <Tab label="Opção A">
-          <div className="sn-grid--bleed-top">Conteúdo de A</div>
-        </Tab>
-        <Tab label="Opção B">
-          <div className="sn-grid--bleed-top">Conteúdo de B</div>
-        </Tab>
-      </Tabs>
-    )
+  const wrapper = mount(
+    <Tabs>
+      <Tab label="Opção A">
+        <div className="sn-grid--bleed-top">Conteúdo de A</div>
+      </Tab>
+      <Tab label="Opção B">
+        <div className="sn-grid--bleed-top">Conteúdo de B</div>
+      </Tab>
+    </Tabs>
+  )
 
+  it('has the second tab with active class', () => {
+    wrapper.setProps({ active: 1 })
+    const currentTab = wrapper.find('.sn-tabs li').last()
+
+    isActive(currentTab)
+  })
+
+  it('shows the second content tab', () => {
+    const expectedContent = 'Conteúdo de B'
+    const currentTab = wrapper.find(Tab).last()
+
+    isShowingContent(currentTab, expectedContent)
+  })
+
+  describe('when change to the first tab', () => {
     it('has the first tab with active class', () => {
+      wrapper.setProps({ active: 0 })
       const currentTab = wrapper.find('.sn-tabs li').first()
 
       isActive(currentTab)
@@ -28,68 +43,6 @@ describe('Tabs', () => {
       const currentTab = wrapper.find(Tab).first()
 
       isShowingContent(currentTab, expectedContent)
-    })
-
-    describe('when click the second tab', () => {
-      beforeEach(() => {
-        wrapper.find('.sn-tabs li a').last().simulate('click')
-      })
-
-      it('has the second tab with active class', () => {
-        const currentTab = wrapper.find('.sn-tabs li').last()
-
-        isActive(currentTab)
-      })
-
-      it('shows the second content tab', () => {
-        const expectedContent = 'Conteúdo de B'
-        const currentTab = wrapper.find(Tab).last()
-
-        isShowingContent(currentTab, expectedContent)
-      })
-    })
-  })
-
-  describe('Custom behavior', () => {
-    const wrapper = mount(
-      <Tabs customBehavior>
-        <Tab label="Opção A">
-          <div className="sn-grid--bleed-top">Conteúdo de A</div>
-        </Tab>
-        <Tab label="Opção B">
-          <div className="sn-grid--bleed-top">Conteúdo de B</div>
-        </Tab>
-      </Tabs>
-    )
-
-    it('has the second tab with active class', () => {
-      wrapper.setProps({ active: 1 })
-      const currentTab = wrapper.find('.sn-tabs li').last()
-
-      isActive(currentTab)
-    })
-
-    it('shows the second content tab', () => {
-      const expectedContent = 'Conteúdo de B'
-      const currentTab = wrapper.find(Tab).last()
-
-      isShowingContent(currentTab, expectedContent)
-    })
-
-    describe('when change to the first tab', () => {
-      it('has the first tab with active class', () => {
-        wrapper.setProps({ active: 0 })
-        const currentTab = wrapper.find('.sn-tabs li').first()
-
-        isActive(currentTab)
-      })
-
-      it('shows the first tab content', () => {
-        const expectedContent = 'Conteúdo de A'
-        const currentTab = wrapper.find(Tab).first()
-
-        isShowingContent(currentTab, expectedContent)
-      })
     })
   })
 })
