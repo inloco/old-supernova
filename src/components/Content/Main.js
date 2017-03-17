9241f798cd1f6e0ff1ba26075f54e1dbf00cdad3
Rafael Nunes <rafael.nunes@inlocomedia.com>
2017-03-17 09:54:50 -0300

    refact(content): refact content component creation

--- src/components/Content/Main.js
+++ src/components/Content/Main.js
@@ -1,17 +1,11 @@
 import React from 'react'
 
-class Content extends React.Component {
-  constructor(props){
-    super(props)
-  }
-
-  render(){
-    return (
-      <main className="sn-layout__content">
-        {this.props.children}
-      </main>
-    )
-  }
+const Content = props => {
+  return (
+    <main className="sn-layout__content">
+      {props.children}
+    </main>
+  )
 }
 
 export default Content
