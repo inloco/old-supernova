f20a8ea12ae59dbda91e6125aa83c5558f126a35
Rafael Nunes <rafael.nunes@inlocomedia.com>
2017-03-28 12:59:22 -0300

    feat(subheader-refact): update folder structure

--- src/components/Subheader/index.js
+++ src/components/Subheader/index.js
@@ -1,6 +1,10 @@
 import React, { PropTypes } from "react"
 
-class Subheader extends React.Component {
+class Subheader extends React.PureComponent {
+  constructor(props){
+    super(props)
+  }
+  
   static propTypes = {
     children: PropTypes.node.isRequired
   }
