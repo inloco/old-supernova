2af4962daa616c86f0cb3857cd31ad3e3f409a9c
Rafael Nunes <rafael.nunes@inlocomedia.com>
2017-03-29 10:32:29 -0300

    feat(subheader-refact): remove subheder constructor #FSRE-40

--- src/components/Subheader/index.js
+++ src/components/Subheader/index.js
@@ -1,10 +1,6 @@
 import React, { PropTypes } from "react"
 
 class Subheader extends React.PureComponent {
-  constructor(props){
-    super(props)
-  }
-  
   static propTypes = {
     children: PropTypes.node.isRequired
   }
