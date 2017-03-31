70a90b15810b0f3ed65494ef6fa5187d67e15763
Bruno Quaresma <bruno.quaresma@inlocomedias-Mac-mini.local>
2017-03-31 12:13:38 -0300

    feat(refact-searchbox): use supernova components to make result card

--- src/components/Searchbox/ResultCard.js
+++ src/components/Searchbox/ResultCard.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Grid from './../Grid'
+import Cell from './../Grid/Cell'
+import Text from './../Text/Main'
 
 class ResultCard extends React.PureComponent {
   static propTypes = {
@@ -18,19 +21,27 @@ class ResultCard extends React.PureComponent {
     return (
       <div onMouseDown={this.handleMouseDown}>
         <div className="sn-search-box__item-content">
-          <div className="sn-grid sn-grid--responsive-mobile-lg sn-grid--cell-gutter">
-            <div className="sn-cell--1">
-              <span className="sn-typo--font-bold">{result.title}</span>
-            </div>
-            <div className="sn-cell--auto sn-cell--text-right">
-              <span className="sn-typo--caption sn-color--secondary-text">{result.info}</span>
-            </div>
-          </div>
-          <div className="sn-grid sn-grid--responsive-mobile-lg">
-            <div className="sn-cell--1">
-              <span className="sn-typo--caption sn-color--secondary-text">{result.subtitle}</span>
-            </div>
-          </div>
+          <Grid responsive="mobile-lg" gutter>
+            <Cell size={1}>
+              <Text span fontWeight="bold">
+                {result.title}
+              </Text>
+            </Cell>
+
+            <Cell size="auto" alignContent="right">
+              <Text span color="sn-color--secondary-text" wrap="caption">
+                {result.info}
+              </Text>
+            </Cell>
+          </Grid>
+
+          <Grid responsive="mobile-lg">
+            <Cell size={1}>
+              <Text span color="sn-color--secondary-text" wrap="caption">
+                {result.subtitle}
+              </Text>
+            </Cell>
+          </Grid>
         </div>
 
         <button type="button" className="sn-search-box__item-button"></button>
