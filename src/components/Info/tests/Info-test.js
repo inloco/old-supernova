7cbc74352a170c99fd971688a820a83404004d67
marina.haack <marina.haack@inlocomedia.com>
2017-04-25 10:09:30 -0300

    refact(Info): changes test name #FSRE-38

--- src/components/Info/tests/Info-test.js
+++ src/components/Info/tests/Info-test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Info from './../index'
 import { shallow } from 'enzyme'
 
-describe('Input', () => {
+describe('Info', () => {
   const expected = 'Some text'
   const wrapper = shallow(<Info text={expected} />)
 
