686fb42305441534743335cd02d29d121dc4ea1d
Tulio Lages <tulio.lages@inlocomedia323.local>
2017-08-21 11:33:27 -0300

    test(Chip): add more tests to increase coverage

--- src/components/Chip/tests/Chip-test.js
+++ src/components/Chip/tests/Chip-test.js
@@ -8,6 +8,84 @@ describe('Chip', () => {
   const wrapper = shallow(
     <Chip
       text='Text'
+    />
+  )
+  
+  it('has text', () => {
+    expect(wrapper.contains('Text')).toBeTruthy()
+  })
+
+  it('has class', () => {
+    expect(wrapper.hasClass('sn-chip')).toBeTruthy()
+  })
+})
+
+describe('Chip with image and icon', () => {
+  const fooFunc = jest.fn()
+
+  const wrapper = shallow(
+    <Chip
+      text='Text'
+      iconCode="home"
+      iconClick={fooFunc}
+      imageUrl="home"
+      imageUrlAltText="alt text"
+    />
+  )
+  
+  it('has text', () => {
+    expect(wrapper.contains('Text')).toBeTruthy()
+  })
+
+  it('has class', () => {
+    expect(wrapper.hasClass('sn-chip')).toBeTruthy()
+  })
+
+  describe('when has icon on the right', () => {
+    it('shows icon', () => {
+      expect(wrapper.find("Icon")).toBeTruthy()
+    })
+
+    it('has default class', () => {
+      expect(wrapper.find("Icon").hasClass('sn-chip__icon')).toBeTruthy()
+    })
+
+    it('has right class', () => {
+      expect(wrapper.find("Icon").hasClass('sn-chip__icon--right')).toBeTruthy()
+    })
+
+    it('has clickable class', () => {
+      expect(wrapper.find("Icon").hasClass('sn-chip__icon--clickable')).toBeTruthy()
+    })
+
+    it('icon executes onClick props when it is clicked', () => {
+      wrapper.find("Icon").simulate('click')
+
+      expect(fooFunc).toBeCalled()
+    })
+  })
+
+  describe('when has image on the left', () => {
+    it('shows image', () => {
+      expect(wrapper.find("img")).toBeTruthy()
+    })
+
+    it('has default class', () => {
+      expect(wrapper.find("img").hasClass('sn-chip__image')).toBeTruthy()
+    })
+
+    it('has left class', () => {
+      expect(wrapper.find("img").hasClass('sn-chip__image--left')).toBeTruthy()
+    })
+  })
+})
+
+describe('Chip with reversed image and icon', () => {
+  const fooFunc = jest.fn()
+
+  const wrapper = shallow(
+    <Chip
+      text='Text'
       textColor="yellow"
       backgroundColor='red'
       iconCode="home"
